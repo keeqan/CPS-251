0afaf36e4fe2595caee0b84ae6f71da1e0af8672
keeqan <keegan.miles20@gmail.com>
2023-10-15 18:25:26 -0400

    update

--- assignments/assignment4_nameSaveData/app/src/main/java/com/ebookfrenzy/namesavedata/MainActivity.kt
+++ assignments/assignment4_nameSaveData/app/src/main/java/com/ebookfrenzy/namesavedata/MainActivity.kt
@@ -8,11 +8,17 @@ import com.ebookfrenzy.namesavedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    //creates the links to binding and to the main view model
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
+
+    //creates a blank variable for updating the text
+
     private val updateText = ""
 
 
+    //on create function
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -23,33 +29,35 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
+        //calls the update name function bringing it to the display
         updateNameDisplay()
 
-        binding.button.setOnClickListener{
 
+        binding.button.setOnClickListener{//checks for button click
+            // checks if the text is empty, if not adds it to the list
             if (binding.editText.text.isNotEmpty()) {
                 viewModel.setName(binding.editText.text.toString())
                 binding.textView.text = viewModel.getName().joinToString( "\n")
 
-            } else {
+            } else { // if there is no name
                 binding.textView.text = "No Name Entered"
             }
 
         }
 
     }
-
+        //creates the save on instance
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(updateText, binding.textView.text.toString())
     }
-
+        //restores the save
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         val savedText = savedInstanceState.getString(updateText)
         binding.textView.text = savedText
     }
-
+        //function that updates the name / and checks if text view is empty
     private fun updateNameDisplay(){
         val nameList = viewModel.getName()
         if (nameList.isNotEmpty()){
--- assignments/assignment4_nameSaveData/app/src/main/java/com/ebookfrenzy/namesavedata/MainViewModel.kt
+++ assignments/assignment4_nameSaveData/app/src/main/java/com/ebookfrenzy/namesavedata/MainViewModel.kt
@@ -3,14 +3,14 @@ package com.ebookfrenzy.namesavedata
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel(){
-
+// puts the names into a list
     private val nameList = ArrayList<String>()
 
-
+// function adds the name
     fun setName(value: String){
         nameList.add(value)
     }
-
+// function that pulls the name list
     fun getName(): List<String> {
         return nameList.toList()
     }
